55eaee11785929fdd46efe9f82020d31fc73e782
Konstantin Zyrianov <kzyryanov@gmail.com>
2025-05-31 23:02:46 +0200

    Enable tvOS and macOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "Navigation",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v17),
+        .macOS(.v15),
+        .tvOS(.v17),
     ],
     products: [
         .library(
